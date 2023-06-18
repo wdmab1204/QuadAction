c0cefc03d78c2bcf34d00378f496e00d1397dadc
wdmab1204 <wdmab1204@naver.com>
2023-06-18 16:11:31 +0900

    ui create

--- Assets/04 Scripts/test.cs
+++ Assets/04 Scripts/test.cs
@@ -1,32 +1,11 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
-using UnityEngine.UI;
-using System;
 
 public class test : MonoBehaviour
 {
-    private void Start()
+    public void Test()
     {
-        Dictionary<int, string> dic = new Dictionary<int, string>();
-
-        dic.Add(1, "one");
-        dic.Add(2, "two");
-
-        int key = 1;
-
-        //key를 이미 가지고있다면
-        if (dic.ContainsKey(key))
-        {
-            //dic[key].Length();
-        }
-
-        foreach(var a in dic)
-        {
-            Debug.Log(a.Key +": "+a.Value);
-        }
-        
+        Debug.Log("UI Box");
     }
 
 }
