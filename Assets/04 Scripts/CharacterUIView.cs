284d90c7e50b1f6dd735351f7b392e91f7da7bf6
wdmab1204 <wdmab1204@naver.com>
2023-06-21 23:32:39 +0900

    SO데이터 기반으로 UIVIew 로직 구현

--- Assets/04 Scripts/CharacterUIView.cs
+++ Assets/04 Scripts/CharacterUIView.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class CharacterUIView : MonoBehaviour
 {
-    public PlayableCharacterModel model;
+    public PlayerCharacterModel model;
     public GameObject boxPrefab;
+
+    CheckBox[] checkboxArray;
     bool isLoaded = false;
+    [SerializeField] CheckBox selectedCheckbox;
     private void OnEnable()
     {
         if (isLoaded) return;
 
+        var characterArray = model.MakeDataFromPlayerModels();
+        checkboxArray = new CheckBox[characterArray.Length];
 
+        for(int i=0; i<characterArray.Length; i++)
+        {
+            var box = Instantiate(boxPrefab);
+            box.transform.parent = this.transform;
+            box.transform.localPosition = new Vector3(box.transform.position.x, box.transform.position.y, 2275);
+            box.transform.localScale = new Vector3(75, 75, 75);
 
+            var characterObject = Instantiate(characterArray[i].Value);
+
+            characterObject.transform.parent = box.transform.GetChild(0);
+            characterObject.transform.localScale = Vector3.one;
+            characterObject.transform.rotation = Quaternion.Euler(-90, 0, 180);
+
+            checkboxArray[i] = box.transform.GetChild(1).GetComponent<CheckBox>();
+            CheckBox myself = checkboxArray[i];
+            checkboxArray[i].CheckboxOn.AddListener(() => CheckboxUpdate(myself));
+        }
+
+        CheckboxUpdate(checkboxArray[0]);
         isLoaded = true;
+    }
+
+    public void CheckboxUpdate(CheckBox select)
+    {
+        foreach (var check in checkboxArray)
+        {
+            if (check != select)
+            {
+                check.Off();
+            }
+        }
+
+        GameObject playerModel = select.transform.parent.GetChild(0).GetChild(0).gameObject;
+        print(playerModel.name);
+        GameData.playableModel.Value = playerModel;
+
+        selectedCheckbox = select;
     }
 }
